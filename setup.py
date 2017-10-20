a9e3c6810fe36967ae9421d14b7ede8bd3f2f9ea
Ben Swift <ben@benswift.me>
2017-10-20 15:18:32 +1100

    update template to c2xt-specific code

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='c2xt',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='command-line tool for translating C header files into xtlang code',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Ben Swift',
+    author_email='ben@benswift.me',
+    url='https://github.com/benswift/c2xt',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
