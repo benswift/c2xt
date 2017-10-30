3dc2a939482e75406d4313de67e6e7a4fcab44c0
Ben Swift <ben@benswift.me>
2017-10-31 09:23:52 +1100

    nicer LLVM_LIB_PATH setup
    preparing for making if more general one day

--- c2xt/__init__.py
+++ c2xt/__init__.py
@@ -1,4 +1,12 @@
 import site
-site.addsitedir("/usr/local/opt/llvm/lib/python2.7/site-packages/")
-import clang.cindex as clang
-clang.Config.set_library_path("/usr/local/opt/llvm/lib/")
+import os
+
+LLVM_LIB_PATH = '/usr/local/opt/llvm/lib'
+site.addsitedir(os.path.join(LLVM_LIB_PATH, 'python2.7', 'site-packages'))
+
+try:
+    import clang.cindex as clang
+    clang.Config.set_library_path(LLVM_LIB_PATH)
+except ImportError:
+    print('Error: could not find LLVM python bindings')
+    exit(1)
