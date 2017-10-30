094692cc9fde8a5223a5162a5b6af1c6e3e153ae
Ben Swift <ben@benswift.me>
2017-10-31 10:18:33 +1100

    add a couple more tests

--- tests/test_nanovg.py
+++ tests/test_nanovg.py
@@ -5,6 +5,10 @@ from c2xt.c2xt import *
 class TestNanoVG:
     nvg = cursor_from_file('tests/nanovg.h', [])
 
+    def test_top_level_walk(self):
+        cursors = [cursor for cursor in self.nvg.get_children() if cursor.spelling == 'NVGwinding']
+        assert cursors
+
     def test_nvgwinding_enum(self):
         cursor = cursor_with_name(self.nvg, 'NVG_SOLID')
         assert cursor.enum_value == 1
