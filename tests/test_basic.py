497828afac5f09efb192aef2d76c8ba3cd213465
Ben Swift <ben@benswift.me>
2017-10-30 18:05:02 +1100

    improve basic test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,4 +4,5 @@ from c2xt.c2xt import *
 
 def test_int_declaration():
     cursor = cursor_from_code_string("int ben = 32;")
-    assert cursor.spelling == "ben"
+    bencursor = cursor_with_name(cursor, "ben")
+    assert bencursor.spelling == "ben"
