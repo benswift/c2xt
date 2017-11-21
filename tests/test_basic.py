6a1a9e9ce4804e6959890ff2d318f3805ac54537
Ben Swift <ben@benswift.me>
2017-11-21 13:03:36 +1100

    refactor test file

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,24 +4,29 @@ from c2xt.c2xt import *
 import c2xt.xtlang as xtlang
 import io
 
+
+def get_test_cursor(code_string, symbol_name):
+    return find_child(parse_code_string(code_string), symbol_name)
+
+
 class TestBasicC:
 
     def test_int_declaration(self):
-        cursor = find_child(parse_code_string('int ben = 32;'), 'ben')
+        cursor = get_test_cursor('int ben = 32;', 'ben')
         assert cursor.spelling == 'ben'
 
     def test_macro_float_literal(self):
-        cursor = find_child(parse_code_string('#define FLOAT_CONST 2.65464f'), 'FLOAT_CONST')
-        assert process_macro_definition(cursor) == '(bind-val FLOAT_CONST float 2.65464 "")'
+        cursor = get_test_cursor('#define FLOAT_CONST 2.65464f', 'FLOAT_CONST')
+        assert xtlang.process_macro_definition(cursor) == '(bind-val FLOAT_CONST float 2.65464 "")'
 
     def test_macro_double_literal(self):
-        cursor = find_child(parse_code_string('#define DOUBLE_CONST 2.6544'), 'DOUBLE_CONST')
-        assert process_macro_definition(cursor) == '(bind-val DOUBLE_CONST double 2.6544 "")'
+        cursor = get_test_cursor('#define DOUBLE_CONST 2.6544', 'DOUBLE_CONST')
+        assert xtlang.process_macro_definition(cursor) == '(bind-val DOUBLE_CONST double 2.6544 "")'
 
     def test_macro_int_literal(self):
-        cursor = find_child(parse_code_string('#define INT_CONST 24'), 'INT_CONST')
-        assert process_macro_definition(cursor) == '(bind-val INT_CONST i32 24 "")'
+        cursor = get_test_cursor('#define INT_CONST 24', 'INT_CONST')
+        assert xtlang.process_macro_definition(cursor) == '(bind-val INT_CONST i32 24 "")'
 
     def test_macro_long_int_literal(self):
-        cursor = find_child(parse_code_string('#define LONG_INT_CONST 24ULL'), 'LONG_INT_CONST')
-        assert process_macro_definition(cursor) == '(bind-val LONG_INT_CONST i64 54 "")'
+        cursor = get_test_cursor('#define LONG_INT_CONST 24ULL', 'LONG_INT_CONST')
+        assert xtlang.process_macro_definition(cursor) == '(bind-val LONG_INT_CONST i64 54 "")'
