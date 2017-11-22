2f0f6258295f276449c61a25abd1ac7816000257
Ben Swift <ben@benswift.me>
2017-11-23 09:49:26 +1100

    add clang-finding stuff to top of c2xt.py
    so that I can run it as a single file thing

--- c2xt/c2xt.py
+++ c2xt/c2xt.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import clang.cindex as clang
-import c2xt.xtlang as xtlang
+import site
+import os
+
+LLVM_LIB_PATH = '/usr/local/opt/llvm/lib'
+site.addsitedir(os.path.join(LLVM_LIB_PATH, 'python2.7', 'site-packages'))
+
+try:
+    import clang.cindex as clang
+    clang.Config.set_library_path(LLVM_LIB_PATH)
+except ImportError:
+    print('Error: could not find LLVM python bindings')
+    exit(1)
+
+import xtlang
 import sys
 
 
