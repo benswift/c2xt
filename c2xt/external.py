0b71c954f4ebe130d43fab3468153cb230d097c2
Ben Swift <ben@benswift.me>
2017-11-28 17:43:43 +1100

    add stb_image process function

--- c2xt/external.py
+++ c2xt/external.py
@@ -12,3 +12,15 @@ def process_nanovg():
         print(xtlang.output_header('nanovg', 'Ben Swift', 'NanoVG bindings for Extempore', ['libs/external/gl.xtm']), file=outfile)
         c2xt.process_file(os.path.join(nanovg_dir, 'src', 'xtmnanovg.c'), 'libnanovg', outfile, [], pre_defined_types, opaque_types)
         print(xtlang.output_footer('nanovg', 'libs/external'), file=outfile)
+
+
+def process_stb_image():
+    nanovg_dir = '/Users/ben/Documents/research/extemporelang/stb'
+    extempore_dir = '/Users/ben/Documents/research/extemporelang/extempore'
+
+    with open(os.path.join(extempore_dir, 'libs', 'external', 'stb_image.xtm'), 'w') as outfile:
+        print(xtlang.output_header('stb_image', 'Ben Swift', 'Sean T. Barrett\'s image library bindings for Extempore', []), file=outfile)
+        c2xt.process_file(os.path.join(nanovg_dir, 'stb_image.h'), 'libstb_image', outfile, ['STBI_FAILURE_USERMSG'], {}, [])
+        c2xt.process_file(os.path.join(nanovg_dir, 'stb_image_resize.h'), 'libstb_image', outfile, ['STBI_FAILURE_USERMSG'], {}, [])
+        c2xt.process_file(os.path.join(nanovg_dir, 'stb_image_write.h'), 'libstb_image', outfile, ['STBI_FAILURE_USERMSG'], {}, [])
+        print(xtlang.output_footer('stb_image', 'libs/external'), file=outfile)
